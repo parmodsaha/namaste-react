83e2a7d0433ae97eb726bb07ba9879b4a83a1ae2
pasaha1188 <pa.saha@easternenterprise.com>
2024-05-21 08:37:59 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+    "h1", 
+    {id: "heading"}, 
+    "Hellow world from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
