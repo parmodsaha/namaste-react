4c75f3eeb234d3e00b4f069dfd33bdda66aea860
pasaha1188 <pa.saha@easternenterprise.com>
2024-06-03 19:45:50 +0530

    script have white space

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 const heading = React.createElement(
     "h1", 
     {id: "heading"}, 
-    "Hellow world from react"
+    "Hellow world from react "
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
