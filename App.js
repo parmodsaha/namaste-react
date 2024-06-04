73f79d969a2b8e00d22765ea02f541de409c0a4c
parmod <parmod.saha@seedwill.co>
2024-06-04 08:30:55 +0530

    react functional component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Hellow world from react"
-);
+import ReactDOM from "react-dom/client";
+
+const jsxheading = (<h1 id="heading">Namaste react from jsx</h1>);
+
+const HeadingComponent = () => {
+    return <h1>this is functional component</h1>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxheading);
