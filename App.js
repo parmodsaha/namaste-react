2285c66c63b389bedd406fb72949fe30510c7459
parmod <parmod.saha@seedwill.co>
2024-06-05 08:48:47 +0530

    jsx used in functional compoinent

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const heading = React.createElement(
-    "h1", 
-    {id: "heading"}, 
-    "Hellow world from react"
+import ReactDOM from "react-dom/client";
+
+const title = (
+    <h1 id="heading" className="head" tabIndex="5">
+       Namaste react using jsx
+    </h1>
+);
+
+const HeadingComponent = () => (
+    <div className="container">
+        {title}
+        <h1>This is functional component</h1>;
+    </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxheading);
+root.render(<HeadingComponent />);
