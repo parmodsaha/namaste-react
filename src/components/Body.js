ad4f11ee7e271624e17535383e12513dae73a0b0
pasaha1188 <pa.saha@easternenterprise.com>
2024-06-19 19:03:30 +0530

    Fixed search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,9 @@ import Shimmer from "./Shimmer";
 const Body = () => {
     //local state variable - super powerful variable
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+    //search box 
+    const [searchText, setSearchText] = useState("");
     useEffect (() => {
         console.log("use effect called");
         fetchData();
@@ -21,6 +24,7 @@ const Body = () => {
         console.log(json);
         // optional chaining
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
     // conditional rendering
     // if(listOfRestaurants.length === 0) {
@@ -31,7 +35,28 @@ const Body = () => {
       <Shimmer />
     ) : (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <div className="search">
+                    <input 
+                    type="text" 
+                    placeholder="Search any" 
+                    className="input-box" 
+                    value={searchText} 
+                    onChange={(e) => { 
+                        setSearchText(e.target.value); 
+                    }} 
+                    />
+                    <button onClick={ () => {
+                        console.log(searchText);
+                        const filteredRestaurant = listOfRestaurants.filter(
+                            (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                        );
+                        setFilteredRestaurant(filteredRestaurant);
+                    }}
+                        >
+                            Search
+                    </button>
+                </div>
                 <button
                     className="filter-btn"
                     onClick={() => {
@@ -44,8 +69,9 @@ const Body = () => {
                 >
                     Top 10 Restaurants
                 </button>
+            </div>
             <div className="res-container">
-                {listOfRestaurants.map((restaurants) => (
+                {filteredRestaurant.map((restaurants) => (
                     <RestaurantCard key={restaurants.info.id} restData={restaurants} />
                 ))}
             </div>
