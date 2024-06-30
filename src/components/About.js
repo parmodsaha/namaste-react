8651ecc57e881bd9ae3d67c6588342c23cdc042c
parmod <parmod.saha@seedwill.co>
2024-06-30 19:27:15 +0530

    class based component with this constructor

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is namaste react</h2>
+            <User name={"Parmod as functional based"} city={"delhi"} contact={"@parmod"} />
+            <UserClass name={"Parmod as class based"} city={"gurgaon"} contact={"@parmod000"} />
         </div>
     );
 };
