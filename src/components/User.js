0ab2c2fe94669f8000337308e60f2d1609f2b2be
pasaha1188 <pa.saha@easternenterprise.com>
2024-07-03 11:16:23 +0530

    class based use useState

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 const User = (props) => {
+    const [count] = useState(0);
+    const [count2] = useState(2);
     return (
         <div className="user-card">
+            <h2>Count: {count2}</h2>
+            <h2>Count: {count}</h2>
             <h2>Name: {props.name}</h2>
             <h3>Location: {props.city}</h3>
             <h4>Contact: {props.contact}</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,19 @@ import React from "react";
 class UserClass extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {
+            count: 0,
+            count2: 1,
+        };
     }
     render () {
         const {name, city, contact} = this.props;
+        const {count} = this.state;
+        const {count2} = this.state;
         return (
             <div className="user-card">
+                <h2>Count: {count}</h2>
+                <h2>Count2: {count2}</h2>
                 <h2>Name: {name}</h2>
                 <h3>Location: {city}</h3>
                 <h4>Contact: {contact}</h4>
